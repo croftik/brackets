edfd78f00867b4e5fe0524849062d0ba0599a66c
croftik <Chayka_Lory@mail.ru>
2020-10-03 18:29:20 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let array = str.split('');
+  let n = array.length / 2;
+  for (let j = 0; j < n; j++){
+    for (let i = 0; i < array.length; i++){
+      if (array[i] == '(' && array[i+1] == ')' || array[i] == '|' && array[i+1] == '|' || array[i] == '[' && array[i+1] == ']' || 
+      array[i] == '{' && array[i+1] == '}' || array[i] == '1' && array[i+1] == '2' || array[i] == '3' && array[i+1] == '4' || 
+      array[i] == '5' && array[i+1] == '6' || array[i] == '7' && array[i+1] == '7' || array[i] == '8' && array[i+1] == '8'){
+        array.splice(i, 2);
+    }
+  }
+}
+  if (array.length == 0) return true;
+  else return false;
 }
